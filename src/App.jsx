499750c4b7bf590de647671495547adf509bb497
unknown <dobsfoxx@gmail.com>
2024-07-03 18:00:32 -0300

    correção código

--- src/App.jsx
+++ src/App.jsx
@@ -7,63 +7,79 @@ function App() {
   //*Componentes são, por convenção com a primeira letra maiúscula.
 
   return (
-    <>
-      <Card_aluno 
-      nomeAluno="Braya" 
-      serie="7° Ano" 
-      media= "4"
-      foto = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdoxeqX4w-_-w7aY9kUVlnln4t6Sn0RHSMEoI6XWo1PCR0IGYgWOEMarGtmlmfxgMBE30&usqp=CAU"
+    
+      <><Card_aluno
+      nomeAluno="Braya"
+      serie="7° Ano"
+      media="4"
+      foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdoxeqX4w-_-w7aY9kUVlnln4t6Sn0RHSMEoI6XWo1PCR0IGYgWOEMarGtmlmfxgMBE30&usqp=CAU"
 
     >
 
-        
-      </Card_aluno>
-      <Card_aluno 
-      nomeAluno="Alana Tonini" 
-      serie="8° Ano" 
-      media= "9"
-      foto = "https://tiinside.com.br/wp-content/uploads/2021/08/marina-Asper-_credito_divulgacao-1-scaled-e1628014859467.jpg"
+
+    </Card_aluno><Card_aluno
+      nomeAluno="Alana Tonini"
+      serie="8° Ano"
+      media="9"
+      foto="https://tiinside.com.br/wp-content/uploads/2021/08/marina-Asper-_credito_divulgacao-1-scaled-e1628014859467.jpg"
     >
+      </Card_aluno><>
+        <Card_aluno
+          nomeAluno="Ketlen"
+          serie="9° Ano"
+          media="7"
+          foto="https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP"
+        >
+        </Card_aluno>
+        <>
 
-        
-      </Card_aluno>
-      <>
-      <Produto
-      nomeProduto = "Ventilador Super Vento, 6 pás Cor Branco"
-      precoOriginal = "R$ 129.90"
-      desconto = {true}
-      precoDesconto = "R$ 103.90"
-      percent = "20% OFF"
-      ></Produto>
-        <Produto
-      nomeProduto = "Máquina de Lavar LavaBem 10 Kg, 8 Regulagens 127v"
-      precoOriginal = "R$ 1499.90"
-      desconto = {false}
-      ></Produto>
-       <Produto
-      nomeProduto = "Geladeira FrostFree 220v, Branco com sistema Inverter"
-      precoOriginal = "R$ 5499.90"
-      desconto = {true}
-      precoDesconto = "R$ 4839.12"
-      percent = "12% OFF"
+          <Produto
+            nomeProduto="Ventilador Super Vento, 6 pás Cor Branco"
+            precoOriginal="R$ 129.90"
+            desconto={true}
+            precoDesconto="R$ 103.90"
+            percent="20% OFF"
+          ></Produto>
+          <Produto
+            nomeProduto="Máquina de Lavar LavaBem 10 Kg, 8 Regulagens 127v"
+            precoOriginal="R$ 1499.90"
+            desconto={false}
+            precoDesconto="R$ 4839.12"
+            percent="12% OFF"
+          ></Produto>
+          <Produto
+            nomeProduto="Geladeira FrostFree 220v, Branco com sistema Inverter"
+            precoOriginal="R$ 5499.90"
+            desconto={true}
+            precoDesconto="R$ 4839.12"
+            percent="12% OFF"
 
-      ></Produto>
-        {/* <Mensagem
-          textoTitulo="As batatas quando nascem se esparramam pelo chão"
-          nomeAutor="J. Almir"
-          paragrafo="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          linkImagem="https://picsum.photos/200"
-        />
-        <Mensagem
-          textoTitulo="As batatas quando nascem, morrem"
-          nomeAutor="Douglas"
-          paragrafo="Ficaram com Deus"
-          linkImagem="https://picsum.photos/200"
-        /> */}
-      </>
-      {/* <Navbar logado={true} nomeUsuario="Ch3rryb0mb"></Navbar> */}
-    </>
-  );
+          ></Produto>
+          <Produto
+            nomeProduto="Freezer horizontal, 220v"
+            precoOriginal="R$ 5499.90"
+            desconto={false}
+            precoDesconto="R$ 4839.12"
+            percent="12% OFF"
+
+          ></Produto>
+          {/* <Mensagem
+      textoTitulo="As batatas quando nascem se esparramam pelo chão"
+      nomeAutor="J. Almir"
+      paragrafo="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      linkImagem="https://picsum.photos/200"
+    />
+    <Mensagem
+      textoTitulo="As batatas quando nascem, morrem"
+      nomeAutor="Douglas"
+      paragrafo="Ficaram com Deus"
+      linkImagem="https://picsum.photos/200"
+    /> */}
+        </>
+        {/* <Navbar logado={true} nomeUsuario="Ch3rryb0mb"></Navbar> */}
+      </></>
+  )
 }
+
 
 export default App;
--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -6,9 +6,8 @@ function Produto(props){
     return(     
        <section className="area">
         <h3 className="anuncio">{props.nomeProduto}</h3>
-        {props.desconto? <h4 className="desconto">{props.precoOriginal}</h4>: <h4 className="sem-desconto">{props.precoOriginal}</h4> }
+        {props.desconto? <><h4 className="desconto">{props.precoOriginal}</h4><h4 className="sem-desconto">{props.precoDesconto}</h4></>: <h4 className="sem-desconto">{props.precoOriginal}</h4> }
         <div className="prices">
-        <h2>{props.precoDesconto}</h2>
         {props.desconto ?<h4 className="porcentagem">{props.percent}</h4>: null}
         </div>
         </section>
